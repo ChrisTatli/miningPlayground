9efd40a9975ad2d4b802e155a41644c9cb44fe9a
Chris Tatli <christopher.tatli@gmail.com>
2022-04-07 19:13:26 +1000

    selfish and fee sniping

--- src/main/java/edu/nyu/crypto/miners/FeeSnipingMiner.java
+++ src/main/java/edu/nyu/crypto/miners/FeeSnipingMiner.java
@@ -5,6 +5,9 @@ import edu.nyu.crypto.blockchain.NetworkStatistics;
 
 public class FeeSnipingMiner extends CompliantMiner implements Miner {
     protected Block currentHead;
+    private double alpha;
+    private double rewardTotal;
+    private int totalRewards;
 
     public FeeSnipingMiner(String id, int hashRate, int connectivity) {
         super(id, hashRate, connectivity);
@@ -23,18 +26,39 @@ public class FeeSnipingMiner extends CompliantMiner implements Miner {
 
 	@Override 
 	public void blockMined(Block block, boolean isMinerMe){
-
+        if(isMinerMe){
+            if(block.getHeight() > currentHead.getHeight()){
+                this.currentHead = block;
+                //this.rewardTotal += block.getBlockValue();
+            }
+        } else {
+            if(block.getHeight() > currentHead.getHeight()){
+                double averageReward = rewardTotal/totalRewards;             
+                if(alpha*alpha*(averageReward+block.getBlockValue())> alpha*averageReward){
+                    this.currentHead = block.getPreviousBlock();
+                    
+                } else {
+                    this.currentHead = block;
+                    //this.rewardTotal += block.getBlockValue();
+                }
+            }
+        }
+        rewardTotal += block.getBlockValue();
+        totalRewards ++;
 	}
+    
 
 	@Override 
 	public void initialize(Block genesis, NetworkStatistics networkStatistics){
 		this.currentHead = genesis;
+        this.rewardTotal = 0;
+        this.totalRewards = 0;
 	}
 
 	@Override
 	public void networkUpdate(NetworkStatistics statistics){
-		
+		this.alpha = this.getHashRate() / (double) statistics.getTotalHashRate();             
 	}
-
 	
 }
+
--- src/main/java/edu/nyu/crypto/miners/SelfishMiner.java
+++ src/main/java/edu/nyu/crypto/miners/SelfishMiner.java
@@ -3,7 +3,9 @@ import edu.nyu.crypto.blockchain.Block;
 import edu.nyu.crypto.blockchain.NetworkStatistics;
 
 public class SelfishMiner extends CompliantMiner implements Miner {
-	protected Block currentHead;
+	protected Block publicHead, privateHead;
+	private int privateBranchLen, delta;
+	private boolean mineSelfish;
 
 	public SelfishMiner(String id, int hashRate, int connectivity) {
 		super(id, hashRate, connectivity);
@@ -11,27 +13,88 @@ public class SelfishMiner extends CompliantMiner implements Miner {
 
 	@Override
 	public Block currentlyMiningAt(){
-		return currentHead;
+		return privateHead;
 	}
 
 	@Override
 	public Block currentHead(){
-		return currentHead;
+		return publicHead;
+	}
+
+
+	public void _blockMined(Block block, boolean isMinerMe){
+		if(isMinerMe){
+			int diffPrev = privateHead.getHeight() - block.getHeight();
+			this.privateHead = block;
+			privateBranchLen++;
+			if(diffPrev == 0 && privateBranchLen == 2 ){
+				this.publicHead = this.privateHead;
+				this.privateBranchLen = 0;
+			}
+		} else {
+			int diffPrev = privateHead.getHeight() - block.getHeight();
+			this.publicHead = block;
+			if (diffPrev == 0){
+				this.privateHead = block;
+				this.privateBranchLen = 0; 
+			} else if (diffPrev == 1){
+				this.publicHead = this.privateHead;
+			} else if (diffPrev == 2){
+				this.publicHead = this.privateHead;
+				this.privateBranchLen = 0;
+			} else {
+				this.publicHead = this.privateHead;
+			}
+		}
 	}
 
 	@Override 
 	public void blockMined(Block block, boolean isMinerMe){
-
+		if(mineSelfish){
+			if(isMinerMe){
+				delta = privateHead.getHeight() - publicHead.getHeight();
+				if(block.getHeight() > privateHead.getHeight()){
+					this.privateHead = block;
+				}
+			} else {
+				if(block.getHeight() > publicHead.getHeight()){
+					this.publicHead = block;
+					delta = privateHead.getHeight() - publicHead.getHeight();
+					if(delta < 0){
+						this.privateHead = this.publicHead;
+					} else if (delta == 0){
+						this.publicHead = this.privateHead;
+					} else if (delta == 1) {
+						this.publicHead = this.privateHead;
+					}
+				}
+			}
+		} else { //compliant 	
+			if(isMinerMe) {
+				if (block.getHeight() > publicHead.getHeight()) {
+					this.publicHead = block;
+					this.privateHead = block;
+				}
+			}
+			else {
+				if (block.getHeight() > publicHead.getHeight()) {
+					this.publicHead = block;
+					this.privateHead = block;
+				}
+			}
+		}
 	}
 
 	@Override 
 	public void initialize(Block genesis, NetworkStatistics networkStatistics){
-		this.currentHead = genesis;
+		this.publicHead = genesis;
+		this.privateHead = genesis;		
 	}
 
 	@Override
 	public void networkUpdate(NetworkStatistics statistics){
-		
+		double alpha = this.getHashRate() / (double) statistics.getTotalHashRate();
+		this.mineSelfish = alpha > 0.25 ? true : false; // better than default if alpha is more than 0,25
 	}
 
 
